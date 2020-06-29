42e8d0508c030c605f88b43c5bdedca751cf0e88
Keith Adorna <keithadorna@gmail.com>
2020-06-29 20:19:51 +0800

    Updates

--- js/main.js
+++ js/main.js
@@ -31,36 +31,3 @@ function toggleMenu() {
     showMenu = false;
   }
 }
-
-var element = $("#square");
-
-// when mouseover execute the animation
-element.mouseover(function () {
-  // the animation starts
-  element.toggleClass("tada animated");
-
-  // do something when animation ends
-  element.one(
-    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-    function (e) {
-      // trick to execute the animation again
-      $(e.target).removeClass("tada animated");
-    }
-  );
-});
-var element = $("#circle");
-
-// when mouseover execute the animation
-element.mouseover(function () {
-  // the animation starts
-  element.toggleClass("flip animated");
-
-  // do something when animation ends
-  element.one(
-    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-    function (e) {
-      // trick to execute the animation again
-      $(e.target).removeClass("flip animated");
-    }
-  );
-});
